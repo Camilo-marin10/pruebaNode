b86c84a759edbece0fd43fc6f51650e8fdea7fcf
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2025-11-11 10:14:09 -0500

    Completamos login, agregamos tokens y todo lo correspondiente a registro y login completo

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-import express from 'express';
-import usuarioRoutes from './routes/usuariosRoutes.js';
-import db from './config/db.js';
+import express from "express";
+import csurf from "csurf";
+import cookieParser from "cookie-parser";
+import usuarioRoutes from "./routes/usuariosRoutes.js";
+import db from "./config/db.js";
 
 //Crear app
 const app = express();
 
 //conexion a la db
 try {
-    await db.authenticate();
-    db.sync();
-    console.log('Conexion correcta a la base de datos');
+  await db.authenticate();
+  db.sync();
+  console.log("Conexion correcta a la base de datos");
 } catch (error) {
-    console.error('No se pudo conectar a la base de datos:', error);
+  console.error("No se pudo conectar a la base de datos:", error);
 }
-
 
 //Habilitar lectura de los forms
 app.use(express.urlencoded({ extended: true }));
 
-//habilitar pug
+// Habilitar el cookie parser
+app.use(cookieParser());
 
-app.set('view engine', 'pug');
-app.set('views', './views');  
+//Habilitar el CSRF
+app.use(csurf({ cookie: true }));
+
+//habilitar pug
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 //definir la ruta public
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //routing
-app.use("/auth", usuarioRoutes)
+app.use("/auth", usuarioRoutes);
 
 //Definir puerto
 const port = process.env.PORT || 3000;
 
 //Definir ruta
 app.listen(port, () => {
-    console.log('El servidor está funcionando en el puerto ' + port);
+  console.log("El servidor está funcionando en el puerto " + port);
 });
